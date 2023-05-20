495630a7c8fd494fbe16b9ec52034d43e866f73d
Rishithachinthakindhi <131257233+Rishithachinthakindhi@users.noreply.github.com>
2023-05-20 18:08:38 +0530

    states.php

---
+++ states.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        .grid-container {
+            display: grid;
+            grid-template-columns: auto auto auto;
+            background-color: aliceblue ;
+            padding: 10px;
+        }
+        .grid-item {
+            background-color: aliceblue;
+            border: 20px solid;
+            padding: 5px;
+            text-align: center;
+            font-size: 10px;
+        }
+    </style>
+</head>
+<body>
+    
+
+    <div class="grid-container">
+        <img src="https://www.tourplan2india.com/wp-content/uploads/2013/01/Taj-Mahal-India.jpg"height="200"width="300">
+        <br><br>
+        <img src="https://1.bp.blogspot.com/-WFQAcrnvrXI/XkjPMkOxc8I/AAAAAAAABcw/sO5TXYkUM7I0xhmTbsVrfrGPvG-xP7hfQCLcBGAsYHQ/s1600/andhra.JPG"height="200"width="300">
+        <br><br>
+        <img src="https://media.gettyimages.com/id/185029695/photo/thirumala-temple.jpg?s=612x612&w=gi&k=20&c=X8ugsZkZ1Auu-wJaSBbMf-8GCozCLgaf4kznRsszgcs="height="200"width="300">
+        <br><br>
+        <img src="https://1.bp.blogspot.com/-UEixoYFftd0/XkjPoHcJoII/AAAAAAAABdQ/NIO9bUh_ICcqpIwNpkp0xGTqpvJUnfhDgCLcBGAsYHQ/s1600/gujrat.JPG"height="200"width="300">
+        <br><br>
+        <img src="https://blogbox.indianeagle.com/wp-content/uploads/2016/02/Jatayu-Nature-Park-in-Kerala-among-new-tourist-attractions-in-India.jpg"height="200"width="300">
+        <br><br>
+        <img src="https://i0.wp.com/indiatourism.travel/wp-content/uploads/adiyogi-coimbatore-tamilnadu-south-india.jpg?resize=1280%2C853&ssl=1"height="200"width="300">
+        </div>
+        
+</body>
+</html>
