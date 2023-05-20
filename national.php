25cd73b5dda9a7a001a14a8c39fd51430ec94f49
Rishithachinthakindhi <131257233+Rishithachinthakindhi@users.noreply.github.com>
2023-05-20 18:05:26 +0530

    national.php

---
+++ national.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        .grid-container {
+            display: grid;
+            grid-template-columns: auto auto auto;
+            background-color: aliceblue ;
+            padding: 10px;
+        }
+        .grid-item {
+            background-color: aliceblue;
+            border: 20px solid;
+            padding: 5px;
+            text-align: center;
+            font-size: 10px;
+        }
+    </style>
+</head>
+<body>
+    
+
+    <div class="grid-container">
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaZ0qSNOuIruLJoM1buMI4cK4wEVWsmA2oFw&usqp=CAU"height="200"width="300">
+        <br><br>
+        <img src="https://www.pelago.co/img/products/AU-Australia/get-high-snorkelling-heli-flight/1012-0356_get-high-snorkelling-heli-flight-australia-pelago1-large.jpg"height="200"width="300">
+        <br><br>
+        <img src="https://alljapantours.com/php17/interfaceMaster/blogMaster/img17/photo/blog/src/COGS193563410.jpg"height="200"width="300">
+        <br><br>
+        <img src="https://lp-cms-production.imgix.net/2021-06/BW5290bb0cd3a8b.jpg?auto=format&q=75&w=1920"height="200"width="300">
+        <br><br>
+        <img src="https://cdn-bmalj.nitrocdn.com/uirOOtSrYrqqUksKHkiSCjZGZlPeXsmk/assets/images/optimized/rev-72e5e55/images/Places-to-visit-in-Mexico-Mexico-City.jpg"height="200"width="300">
+        <br><br>
+        <img src="https://www.godigit.com/content/dam/godigit/directportal/en/contenthm/sugar-loaf-rio-de-janeiro.jpg"height="200"width="300">
+        </div>
+        
+</body>
+</html>
