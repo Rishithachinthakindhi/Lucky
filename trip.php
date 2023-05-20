bcc496454f3580d3ac715b8faf84e0dd4464c974
Rishithachinthakindhi <131257233+Rishithachinthakindhi@users.noreply.github.com>
2023-05-20 17:20:51 +0530

    trip.php

---
+++ trip.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=>, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+    <title>Document</title>
+    <style>
+        body{
+            background-image:url('https://www.fenixtours.co.in/images/background/about.jpg');
+            background-size:cover;
+        }
+    </style>
+</head>
+<body>
+    <nav class="navbar navbar-expand-lg sticky-top  " id="navbar">
+        <div class="container-fluid">
+            <a href="#"><img src="https://scontent.fhyd2-1.fna.fbcdn.net/v/t39.30808-6/304961278_441810701343392_5162196941967253822_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=e3f864&_nc_ohc=hSgKwKyBPnIAX_8ZJsH&_nc_ht=scontent.fhyd2-1.fna&oh=00_AfB_f8sbW-gEaFr1OvyKvka33OvAdU48Nrl5gd-tZmqpJw&oe=646D06D7"width="80" height="100" ></a>
+          
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+              
+             
+              <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                  <b>Trips</b>
+                </a>
+                <ul class="dropdown-menu" aria-labelledby="navbarDropdown" style="background-color:aquamarine;">
+                  
+                  <li><a class="dropdown-item" href="national.php">National</a></li>
+                  
+                  <li><a class="dropdown-item" href="states.php">States</a></li>
+                 
+                </ul>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="student-create.php"><b>User Login</b></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="index.php"><b>Admin Login</b></a>
+              </li>
+            </ul>
+            
+          </div>
+        </div>
+    </nav>
+    
+    
+  </div>
+  
+  
+    
+            </div>
+        </div>
+    </section>
+</body>
+</html>
